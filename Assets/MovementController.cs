d59a96a491eaeb0253ffa314f7546482a82577a9
SkyanSam <31390605+SkyanSam@users.noreply.github.com>
2025-04-15 13:48:15 -0400

    movement wowowowowow

---
+++ Assets/MovementController.cs
@@ -0,0 +1,28 @@
+using System;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    public GameObject cam;
+    public float speed;
+    public float minTurnAngle = 30.0f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var angle = cam.transform.rotation;
+        if (angle.x < -Mathf.Deg2Rad * minTurnAngle) { 
+            // TURN LEFT
+        } else if (angle.x > Mathf.Deg2Rad * minTurnAngle) {
+            // TURN RIGHT
+        } else {
+            // STAY STILL
+        }
+    }
+}
