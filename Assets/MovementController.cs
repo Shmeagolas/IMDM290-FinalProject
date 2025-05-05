b18688f7bd6d7b274a564eca5b41892e70795c1d
SkyanSam <samuelwiggins15@gmail.com>
2025-05-04 20:28:34 -0400

    Working Speed and Head Rotation

--- Assets/MovementController.cs
+++ Assets/MovementController.cs
@@ -1,15 +1,22 @@
 using System;
 using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class MovementController : MonoBehaviour
 {
     public GameObject cam;
-    public float speed;
-    public float minTurnAngle = 30.0f;
+    public float maxSpeed;
+    float speed;
+    public float accel;
+    public float decel;
+    public float minDiff = 0.01f;
+    public float minTurnAngle = 10.0f;
     public GameObject leftController;
     public GameObject rightController;
     public TMPro.TextMeshProUGUI tmpro;
+    Vector3 lastLeftPos;
+    Vector3 lastRightPos;
     public enum TurnMode
     {
         Left, Right, Straight
@@ -24,13 +31,34 @@ public class MovementController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        var angle = cam.transform.rotation;
-        if (angle.x < -Mathf.Deg2Rad * minTurnAngle) {
-            turnMode = TurnMode.Left;
-        } else if (angle.x > Mathf.Deg2Rad * minTurnAngle) {
-            turnMode = TurnMode.Right;
-        } else {
+        float diff = Mathf.Abs((lastLeftPos - leftController.transform.position).magnitude) + Mathf.Abs((lastRightPos - rightController.transform.position).magnitude) * Time.deltaTime / 2f;
+        var angle = cam.transform.parent.localEulerAngles;
+        //tmpro.text = $"turnmode: {(turnMode == TurnMode.Left ? "left" : turnMode == TurnMode.Right ? "right" : "straight")}\n angle: {angle.x},{angle.y},{angle.z}, diff: {diff}";
+        tmpro.text = $"turnmode: {(turnMode == TurnMode.Left ? "left" : turnMode == TurnMode.Right ? "right" : "straight")} speed: {speed}";
+        if (angle.z < minTurnAngle || angle.z > 360 - minTurnAngle)
+        {
             turnMode = TurnMode.Straight;
         }
+        else if (angle.z > 180) 
+        {
+            turnMode = TurnMode.Right;
+        } 
+        else if (angle.z < 180)  
+        {
+            turnMode = TurnMode.Left;
+        }
+
+        if (diff >= minDiff)
+        {
+            speed += accel * Time.deltaTime;
+            speed = Mathf.Min(speed, maxSpeed);
+        }
+        else
+        {
+            speed -= decel * Time.deltaTime;
+            speed = Mathf.Max(speed, 0);
+        }
+        lastLeftPos = leftController.transform.position;
+        lastRightPos = rightController.transform.position;
     }
 }
